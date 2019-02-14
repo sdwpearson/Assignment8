be58edd10edc5c8f4ef1fc57b7f6ca854b29da85
Ramses van Zon <rzon@scinet.utoronto.ca>
2019-02-14 09:23:18 -0500

    Added testing scaffold cc files

---
+++ initializationtest.cc
@@ -0,0 +1,10 @@
+// initializationtest.cc
+
+#include "initialization.h"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE initializationunittest
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(initializationtest) {
+}
