7579eb61f48735fdd5ec1530a27148813867d92c
sdwpearson <sdwpearson@gmail.com>
2019-03-24 20:34:46 -0400

    a

--- write_subjobs.py
+++ write_subjobs.py
@@ -5,7 +5,7 @@ with open("subjobs", "w+") as output_file:
 		for seed in range(1,41):
 			#length = i/50*50+100
 			#seed = i%40+1
-			output_file.write("mkdir -p serialjobdir%d && cd serialjobdir%d && ../antsontable --filename=ants%d.nc --length=%d --seed=%d; echo \"Job %d Done\" \r\n" %(i, i, i, length, seed, i)) 
+			output_file.write("mkdir -p serialjobdir%d; cd serialjobdir%d; ../antsontable --filename=ants%d.nc --length=%d --seed=%d; echo \"Job %d Done\" \r\n" %(i, i, i, length, seed, i)) 
 			i = i + 1;
 output_file.close()
 
