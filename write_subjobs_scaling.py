0f98201eb9110e89b173261a98e0e13a0a6e7e03
sdwpearson <sdwpearson@gmail.com>
2019-03-24 21:30:28 -0400

    a

---
+++ write_subjobs_scaling.py
@@ -0,0 +1,12 @@
+
+
+for core in range(1,17):
+	i = 1;
+	filename = "subjobs"+str(core)
+	with open(filename, "w+") as output_file:	
+		for length in range(100,350,50):
+			for seed in range(1,41):
+				output_file.write("./antsontable --netcdf_output_steps=0 --screen_output_steps=0 --length=%d --seed=%d; echo \"Job %d Done\" \r\n" %(length, seed, i)) 
+				i = i + 1;
+output_file.close()
+
