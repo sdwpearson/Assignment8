fdf7b8a31b68dce0870550a2c8739796c54f9ed7
sdwpearson <sdwpearson@gmail.com>
2019-03-24 21:19:36 -0400

    a

--- write_subjobs.py
+++ write_subjobs.py
@@ -3,7 +3,7 @@ i = 1;
 with open("subjobs", "w+") as output_file:	
 	for length in range(100,350,50):
 		for seed in range(1,41):
-			output_file.write("../antsontable --length=%d --seed=%d; echo \"Job %d Done\" \r\n" %(length, seed, i)) 
+			output_file.write("./antsontable --length=%d --seed=%d; echo \"Job %d Done\" \r\n" %(length, seed, i)) 
 			i = i + 1;
 output_file.close()
 
